938fe6b282c8359d58a871d171406a7437a1f15a
Satoshi Ebisawa <e.satoshi@gmail.com>
2014-08-18 00:09:36 +0900

    Receive scopes / Look for already existing authorization

--- client/client.go
+++ client/client.go
@@ -9,6 +9,7 @@ import (
 	"net/url"
 	"os"
 	"os/user"
+	"regexp"
 	"strings"
 
 	"code.google.com/p/goauth2/oauth"
@@ -18,22 +19,23 @@ import (
 )
 
 // CreateClient は認証済みの github.Client を返します
-func CreateClient(appName string) *github.Client {
+func CreateClient(appName string, scopes []string) *github.Client {
 	// 設定ファイルがあれば読み込む
 	var accessToken string
 	var err error
 
 	user, _ := user.Current()
-	conf := user.HomeDir + "/." + strings.ToLower(appName) + ".conf"
-	buf, err := ioutil.ReadFile(conf)
+	confName := "." + strings.ToLower(appName) + ".conf"
+	confpath := user.HomeDir + "/" + confName
+	buf, err := ioutil.ReadFile(confpath)
 	if err != nil {
 		// 認証して accessToken を取得する
-		accessToken, err = fetchAccessToken(appName)
+		accessToken, err = fetchAccessToken(appName, scopes)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		ioutil.WriteFile(conf, []byte(accessToken), 0644)
+		ioutil.WriteFile(confpath, []byte(accessToken), 0644)
 	} else {
 		// ファイルから accessToken を読み込む
 		accessToken = fmt.Sprintf("%s", buf)
@@ -49,7 +51,7 @@ func oauthClient(accessToken string) *github.Client {
 	return github.NewClient(t.Client())
 }
 
-func fetchAccessToken(appName string) (string, error) {
+func getCredentials() (string, string, string) {
 	scan := func() string {
 		reader := bufio.NewReader(os.Stdin)
 		input, _ := reader.ReadString('\n')
@@ -63,21 +65,87 @@ func fetchAccessToken(appName string) (string, error) {
 	fmt.Print("Two Factor Auth: ")
 	tfaToken := scan()
 
-	payload := struct {
-		Scopes []string `json:"scopes"`
-		Note   string   `json:"note"`
-	}{
-		Scopes: []string{"repo", "public_repo", "read:org"},
-		Note:   appName,
+	return login, password, tfaToken
+}
+
+// 既に appName という名前のトークンがあるかどうか探す
+func findAccessToken(s *napping.Session, appName string) (string, error) {
+	type authorization struct {
+		ID        int
+		URL       string
+		Scopes    []string
+		Token     string
+		App       map[string]string
+		Note      string `json:"note"`
+		NoteURL   string `json:"note_url"`
+		UpdatedAt string `json:"updated_at"`
+		CreatedAt string `json:"created_at"`
+	}
+	res := []authorization{}
+
+	e := struct {
+		Message string
+		Errors  []struct {
+			Resource string
+			Field    string
+			Code     string
+		}
+	}{}
+
+	fetchAuthorizations := func(url string) ([]authorization, string, error) {
+		resp, err := s.Get(url, nil, &res, &e)
+		if err != nil {
+			return nil, "", err
+		}
+		if resp.Status() == 200 {
+			linkStr := resp.HttpResponse().Header["Link"][0]
+			reg, _ := regexp.Compile("\\<(.*)?\\>; rel=\"next\"")
+			subMatch := reg.FindStringSubmatch(linkStr)
+			var nextURL string
+			if len(subMatch) > 0 {
+				nextURL = subMatch[1]
+			} else {
+				nextURL = ""
+			}
+			return res, nextURL, nil
+		}
+		return nil, "", errors.New("Failed to fetch Authentications.")
 	}
 
+	authorizations := []authorization{}
+	url := "https://api.github.com/authorizations"
+	for {
+		res, nextURL, err := fetchAuthorizations(url)
+		if err != nil {
+			return "", nil
+		}
+		if nextURL == "" {
+			break
+		}
+		url = nextURL
+		authorizations = append(authorizations, res...)
+	}
+
+	for i := 0; i < len(authorizations); i++ {
+		// "hoge (API)" という名前が付けられるようなのでそれで探す
+		// FIXME: note 属性で探したかったが、レスポンスについてこなかった
+		if authorizations[i].App["name"] == appName+" (API)" {
+			return authorizations[i].Token, nil
+		}
+	}
+
+	return "", nil
+}
+
+// 新しくトークンを作る
+func createAccessToken(s *napping.Session, appName string, scopes []string) (string, error) {
 	res := struct {
 		ID        int
 		URL       string
 		Scopes    []string
 		Token     string
 		App       map[string]string
-		Note      string
+		Note      string `json:"note"`
 		NoteURL   string `json:"note_url"`
 		UpdatedAt string `json:"updated_at"`
 		CreatedAt string `json:"created_at"`
@@ -92,16 +160,15 @@ func fetchAccessToken(appName string) (string, error) {
 		}
 	}{}
 
-	header := &http.Header{}
-	header.Add("X-GitHub-OTP", tfaToken)
-
-	s := napping.Session{
-		Userinfo: url.UserPassword(login, password),
-		Header:   header,
+	payload := struct {
+		Scopes []string `json:"scopes"`
+		Note   string   `json:"note"`
+	}{
+		Scopes: scopes,
+		Note:   appName,
 	}
 
 	url := "https://api.github.com/authorizations"
-
 	resp, err := s.Post(url, &payload, &res, &e)
 	if err != nil {
 		return "", err
@@ -114,5 +181,41 @@ func fetchAccessToken(appName string) (string, error) {
 	fmt.Printf("\t Status:  %v\n", resp.Status())
 	fmt.Printf("\t Message: %v\n", e.Message)
 	fmt.Printf("\t Errors: %v\n", e.Message)
-	return "", errors.New("既に " + appName + " という Personal API Token が存在する場合は削除してください。")
+	return "", errors.New("Failed to create Access Token.")
+}
+
+func fetchAccessToken(appName string, scopes []string) (string, error) {
+	login, password, tfaToken := getCredentials()
+
+	// Two Factor Auth の認証をするため一度ダミーでリクエストする
+	// https://github.com/github/hub/blob/master/lib/hub/github_api.rb#L338
+	if tfaToken != "" {
+		dummySession := napping.Session{
+			Userinfo: url.UserPassword(login, password),
+		}
+		url := "https://api.github.com/authorizations"
+		dummySession.Post(url, nil, nil, nil)
+	}
+
+	header := &http.Header{}
+	header.Add("X-GitHub-OTP", tfaToken)
+
+	s := napping.Session{
+		Userinfo: url.UserPassword(login, password),
+		Header:   header,
+	}
+
+	foundToken, err := findAccessToken(&s, appName)
+	if err != nil {
+		return "", err
+	}
+	if foundToken != "" {
+		return foundToken, nil
+	}
+
+	createdToken, err := createAccessToken(&s, appName, scopes)
+	if err != nil {
+		return "", err
+	}
+	return createdToken, nil
 }
